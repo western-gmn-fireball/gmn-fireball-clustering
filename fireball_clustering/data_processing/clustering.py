8f0544fe1f86526343f281edda465da1bf5cdb87
Armaan Mahajan <amahaj5@uwo.ca>
2025-01-22 15:08:02 -0500

    WIP: Worked on peak detection algorithm, currently is untested

--- fireball_clustering/data_processing/clustering.py
+++ fireball_clustering/data_processing/clustering.py
@@ -4,52 +4,57 @@
     Date: 2025-01-30
 '''
 import numpy as np
+import sqlite3
+
+from ..database import db_writes 
 
 '''
-    @todo: Correct to work with new data format and implement DB usage
+    @todo: Add lat and lng into the data considerations (could just include it in station_data to have it for reference everywhere)
 '''
-def identifyFireballs(station_data, save_to_db=True):
+def identifyFireballs(station_name, station_data, save_to_db=True):
     '''
     Args:
         station_data (dict): Dictionary with processed station data.
         save_to_db (bool): If True, saves the fireball data to the database.
     '''
     first_timestamp = min(station_data['datetimes'])
-    features = []
-    for station, data in datasets.items():
+    fireballs = []
+        
+    # Get standard deviation
+    std = np.std(station_data['detrended_intensities'])
 
-        # Get standard deviation
-        std = np.std(data['detrended_intensities'])
+    # Modify into feature space
+    up = True
+    down = False
+    event = [] # (start_time, end_time, station_id)
+    '''
+        Iterates through the intensities
+        Keep track of when the std is crossed in both directions
+        Each spike (i.e in between an increasing slope cross of std and 
+        decreasing slope cross of std) is added to the feature set
+    '''
+    for idx in range(len(station_data['intensities'])):
+        if station_data['detrended_intensities'][idx] >= 2*std and up:
+            up = False
+            down = True
 
-        # Modify into feature space
-        up = True
-        down = False
-        feature = [] # (start_time, end_time, station_id)
-        '''
-            Iterates through the intensities
-            Keep track of when the std is crossed in both directions
-            Each spike (i.e in between an increasing slope cross of std and 
-            decreasing slope cross of std) is added to the feature set
-        '''
-        for idx in range(len(data['intensities'])):
-            if data['detrended_intensities'][idx] >= 2*std and up:
-                up = False
-                down = True
+            time_seconds = (station_data['datetimes'][idx] - first_timestamp).total_seconds()
+            event.append(time_seconds)
+        if station_data['detrended_intensities'][idx] <= 2*std and down:
+            down = False
+            up = True
 
-                time_seconds = (data['datetimes'][idx] - first_timestamp).total_seconds()
-                feature.append(time_seconds)
-            if data['detrended_intensities'][idx] <= 2*std and down:
-                down = False
-                up = True
+            time_seconds = (station_data['datetimes'][idx] - first_timestamp).total_seconds()
+            event.append(time_seconds)
+            event.append(station_name)
+            fireballs.append(event)
+            
+            event = []
 
-                time_seconds = (data['datetimes'][idx] - first_timestamp).total_seconds()
-                feature.append(time_seconds)
-                feature.append(station)
-                features.append(feature)
-                
-                feature = []
+    # Add fireball events to the DB
+    db_writes.insertFireballs([(station_name, start_time, end_time) for start_time, end_time, station_name in fireballs])
 
-    return features
+    return fireballs 
 
 def clusterFireballs():
     pass
