d60f7a6011f0e3356858db1427432ef538efe5b3
Armaan <amahaj5@uwo.ca>
2025-01-22 08:37:23 -0500

    Create tables functions

--- fireball_clustering/data_processing/clustering.py
+++ fireball_clustering/data_processing/clustering.py
@@ -3,9 +3,53 @@
     Author: Armaan Mahajan
     Date: 2025-01-30
 '''
+import numpy as np
 
-def createFeatureSet():
-    pass
+'''
+    @todo: Correct to work with new data format and implement DB usage
+'''
+def identifyFireballs(station_data, save_to_db=True):
+    '''
+    Args:
+        station_data (dict): Dictionary with processed station data.
+        save_to_db (bool): If True, saves the fireball data to the database.
+    '''
+    first_timestamp = min(station_data['datetimes'])
+    features = []
+    for station, data in datasets.items():
+
+        # Get standard deviation
+        std = np.std(data['detrended_intensities'])
+
+        # Modify into feature space
+        up = True
+        down = False
+        feature = [] # (start_time, end_time, station_id)
+        '''
+            Iterates through the intensities
+            Keep track of when the std is crossed in both directions
+            Each spike (i.e in between an increasing slope cross of std and 
+            decreasing slope cross of std) is added to the feature set
+        '''
+        for idx in range(len(data['intensities'])):
+            if data['detrended_intensities'][idx] >= 2*std and up:
+                up = False
+                down = True
+
+                time_seconds = (data['datetimes'][idx] - first_timestamp).total_seconds()
+                feature.append(time_seconds)
+            if data['detrended_intensities'][idx] <= 2*std and down:
+                down = False
+                up = True
+
+                time_seconds = (data['datetimes'][idx] - first_timestamp).total_seconds()
+                feature.append(time_seconds)
+                feature.append(station)
+                features.append(feature)
+                
+                feature = []
+
+    return features
 
 def clusterFireballs():
     pass
---
+++ fireball_clustering/database/db_setup.py
@@ -0,0 +1,58 @@
+'''
+    This file contains functions to create and initialize the database with
+    the following tables:
+        Stations:
+            - station_id (int): Unique identifier for the station.
+            - latitude (real): Latitude of the station
+            - longitude (real): Longitude of the station
+        Fireballs:
+            - fireball_id (int): Unique identifier for the fireball
+            - station_id (FOREIGN KEY INT): Identifier of observing station
+            - start_time (TEXT): ISO8601 representation of fireball start_time
+            - end_time (TEXT): ISO8601 representation of fireball end_time
+        Clusters:
+            - cluster_id (PRIMARY KEY INT): Unique identifier for the cluster
+            - start_time (TEXT): ISO8601 representation of cluster start_time
+            - end_time (TEXT): ISO8601 representation of cluster end_time
+        Cluster_Fireballs:
+            - fireball_id (FOREIGN KEY INT)
+            - cluster_id (FOREIGN KEY INT) 
+'''
+
+import sqlite3
+
+def initializeEmptyDatabase():
+    # Initialize connection
+    con = sqlite3.connect('gmn_fireball_clustering.db')
+    cursor = con.cursor()
+
+    # Create tables
+    cursor.execute("""
+                   CREATE TABLE stations(
+                        station_id INTEGER PRIMARY KEY,
+                        latitude REAL NOT NULL,
+                        longitude REAL NOT NULL 
+                   )
+                   """)
+    cursor.execute("""
+                   CREATE TABLE fireballs(
+                        fireball_id INTEGER PRIMARY KEY,
+                        FOREIGN KEY (station_id) REFERENCES station(station_id)
+                        start_time TEXT,
+                        end_time TEXT
+                   )
+                   """)
+    cursor.execute("""
+                   CREATE TABLE stations(
+                        cluster_id INTEGER PRIMARY KEY,
+                        start_time TEXT,
+                        end_time TEXT
+                   )
+                   """)
+    cursor.execute("""
+                   CREATE TABLE fireballs(
+                        FOREIGN KEY (cluster_id) REFERENCES clusters(cluster_id)
+                        FOREIGN KEY (fireball_id) REFERENCES fireballs(fireball_id)
+                   )
+                   """)
+    con.commit()
