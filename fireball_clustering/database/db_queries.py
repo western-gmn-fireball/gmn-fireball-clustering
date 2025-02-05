4045431a3c1a8479a21afdd3e374224d31a3bfd5
Armaan <amahaj5@uwo.ca>
2025-02-04 20:44:01 -0500

    Added database queries to get stations given station IDs

--- fireball_clustering/database/db_queries.py
+++ fireball_clustering/database/db_queries.py
@@ -3,3 +3,23 @@
 
     Author: Armaan Mahajan
 '''
+import sqlite3
+
+def getStations(station_ids):
+    '''
+    Fetches the station data from the database for a given set of station IDs.
+
+    Args:
+        station_ids (str[]): An array of station ID strings
+    
+    Returns:
+        An array of tuples with form (station_id, latitude, longitude)
+    '''
+    placeholders = ",".join('?' for _ in station_ids) 
+    QUERY = f"SELECT * FROM stations WHERE station_id IN ({placeholders})"
+
+    conn = sqlite3.connect('gmn_fireball_clustering.db')
+    cur = conn.cursor()
+    stations = cur.execute(QUERY, station_ids)
+    res = [station for station in stations]
+    return res
